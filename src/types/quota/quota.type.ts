1b4f142b8601a6fb74fde8eef32b362706110e95
Capt. Michael <XinwenCheng@Live.com>
2023-04-23 01:04:55 +0800

    perf: improve quota data type

--- src/types/quota/quota.type.ts
+++ src/types/quota/quota.type.ts
@@ -1,16 +1,19 @@
 /**
  * Quota data type, used to define the quota data structure.
- * 0 means no quota, -1 means no limit.
+ * 0 or undefined means no quota, -1 means no limit.
  */
 export type QuotaDataType = {
   id?: string;
+  userId: string;
   subject: string;
   task: {
     maxNewAmountMonthly: number;
   };
   storage: {
     maxSizeInBytes: number;
+    usedSizeInBytes?: number;
   };
+  isDeleted?: boolean;
   createdAt?: Date;
   updatedAt?: Date;
 };
