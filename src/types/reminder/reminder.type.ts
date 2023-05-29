a0a4c6b03358a846ee200d40ba4ab723b0b69117
Capt. Michael <XinwenCheng@Live.com>
2023-05-29 13:22:48 +0800

    feat: data type of remind at, for avoiding timezone offset

--- src/types/reminder/reminder.type.ts
+++ src/types/reminder/reminder.type.ts
@@ -4,3 +4,26 @@ export type ReminderDataType = {
   subject: string;
   content?: string;
 };
+
+export type RemindAtDataType = {
+  /**
+   * Year.
+   */
+  year: number;
+  /**
+   * Month, 0-11.
+   */
+  month: number;
+  /**
+   * Day of month, 1-31.
+   */
+  date: number;
+  /**
+   * Hour, 0-23.
+   */
+  hour: number;
+  /**
+   * Minute, 0-59.
+   */
+  minute: number;
+};
